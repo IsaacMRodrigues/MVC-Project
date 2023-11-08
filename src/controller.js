623ab699aedabf68c29c45a7a7bf2141f8920b99
IsaacMRodrigues <isaacmrodrigues2019@gmail.com>
2023-11-07 21:27:56 -0300

    função delete

--- src/controller.js
+++ src/controller.js
@@ -9,18 +9,32 @@ let submitState = submitType.NEW;
 let currentId = null;
 
 
+function validaLogin(nome) {
+  return data.some(data => data.nome === nome);
+}
+
+
 const handleSubmit = (event) => {
   event.preventDefault();
+
+  if(nome.value == "" || idade.value == "" || login.value == "" || senha.value == ""){
+    alert("Preencha todos os campos")
+  } else if(validaLogin(login.value) && submitState == submitType.NEW){
+    alert("Esse Login já está sendo utilizado");
+  } else {
+
   const user = new Usuario(nome.value, idade.value, login.value, senha.value);
   if (submitState == submitType.NEW){
     addUser(user);
   } else if (submitState == submitType.UPDATE){
     updateUser(currentId, user);
     submitState = submitType.NEW;
-    btnSub.innerText = "Adicionar";
+    login.disabled = false;
+    btnSub.innerText = "Salvar";
     resultView.update(user);
   }
   viewController.update(data, new Usuario("", "", "", ""))
+}
 };
 
 const addUser = (newUser) => {
@@ -41,9 +55,9 @@ const clickEsquerdo = (event) => {
   currentId =  event.target.closest('tr').id.split("")[4];
   alert(`${data[currentId].getNome().toUpperCase()} sera carregado para edição`);
   viewController.updateForm(data[currentId])
+  login.disabled = true;
   submitState = submitType.UPDATE;
   btnSub.innerText = "Atualizar";
-
 
 }
 
@@ -52,8 +66,11 @@ const clickDireito = (event) => {
   event.preventDefault();
   if(event.button == 2) {
     
-    currentId =  event.target.closest('tr').id.split("")[4];
-    alert(`${data[currentId].getNome().toUpperCase()} sera deletado`)
+    currentId =  parseInt (event.target.closest('tr').id.split("")[4]);
+    alert(`${data[currentId].getNome().toUpperCase()} sera deletado ${currentId}`)
+    deleteUser(currentId);
+    resultView.update(data);
+
   }
 }
 
